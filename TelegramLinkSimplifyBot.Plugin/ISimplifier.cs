28f947faf457467e8ed92959d55a8d4d56094903
Gaein nidb <mail@gaein.cn>
2023-12-10 00:11:21 +0800

    feat: add plugin interface

---
+++ TelegramLinkSimplifyBot.Plugin/ISimplifier.cs
@@ -0,0 +1,8 @@
+﻿namespace TelegramLinkSimplifyBot.Plugin;
+
+internal interface ISimplifier
+{
+    public string Name { get; set; }
+    public string Verison { get; set; }
+    public IDictionary<string, Func<Task<(bool, string, Uri)>, Uri>> SimplifyMethods { get; set; }
+}
