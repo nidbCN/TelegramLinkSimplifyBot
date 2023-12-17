7b0dd0fc749ecabe178036b5f60422c2e0175e77
Gaein nidb <mail@gaein.cn>
2023-12-18 00:43:53 +0800

    feat: add item.jd.com simplifier

---
+++ TelegramLinkSimplifyBot.Plugins.Builtin/Simplifiers/JingDongSimplifier.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelegramLinkSimplifyBot.Plugin;
+
+namespace TelegramLinkSimplifyBot.Plugins.Builtin.Simplifiers;
+public class JingDongSimplifier : ISimplifier
+{
+    public string Name => "JDSimplifier";
+
+    public string Version => "6.0.0";
+
+    public IDictionary<string, Func<Uri, Task<(bool, string?, Uri?)>>> SimplifyMethods
+        => new Dictionary<string, Func<Uri, Task<(bool, string?, Uri?)>>>()
+        {
+            {"item.jd.com", SimplifyNormalUrl }
+        };
+
+    private async Task<(bool, string?, Uri?)> SimplifyNormalUrl(Uri origin)
+    {
+        var cleardUrl = await Task.Run(() => new Uri(origin, origin.AbsolutePath));
+
+        return (true, null, cleardUrl);
+    }
+}
